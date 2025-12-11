2df928cc84f0166b73496e56adb474579fc12697
aya <ayaloaymohammad@gmail.com>
2025-12-11 13:47:44 +0300

    add texture on earth and moon

--- src/main.cpp
+++ src/main.cpp
@@ -7,29 +7,47 @@
 
 #include "../include/shader.h"
 #include "../include/camera.h"
-#include "../include/config.h"
 #include "../include/planet.h"
-#include "../include/scenario.h"
 
 #include <iostream>
 #include <vector>
-#include <map>
-#include <string>
-#include <algorithm>
 
 // ===================== Globals =====================
 unsigned int SCR_WIDTH = 1280, SCR_HEIGHT = 720;
 Camera camera(glm::vec3(0.0f, 5.0f, 20.0f));
-float lastX = 640, lastY = 360;
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
-float deltaTime = 0.0f, lastFrame = 0.0f;
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
 
-// ===================== Input Callbacks =====================
-void processInput(GLFWwindow* window)
+// ===================== Callbacks =====================
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-    float currentFrame = glfwGetTime();
-    deltaTime = currentFrame - lastFrame;
-    lastFrame = currentFrame;
+    glViewport(0,0,width,height);
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if(firstMouse){ lastX = xpos; lastY = ypos; firstMouse = false; }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    camera.ProcessMouseScroll(yoffset);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window,true);
 
     if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
         camera.ProcessKeyboard(FORWARD, deltaTime);
@@ -41,145 +59,26 @@ void processInput(GLFWwindow* window)
         camera.ProcessKeyboard(RIGHT, deltaTime);
 }
 
-void mouse_callback(GLFWwindow* window, double xpos, double ypos)
-{
-    if(firstMouse)
-    {
-        lastX = xpos;
-        lastY = ypos;
-        firstMouse = false;
-    }
-    float xoffset = xpos - lastX;
-    float yoffset = lastY - ypos; // inverted
-    lastX = xpos;
-    lastY = ypos;
-    camera.ProcessMouseMovement(xoffset, yoffset);
-}
-
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
-{
-    camera.ProcessMouseScroll(yoffset);
-}
-
-// ===================== Skybox Helpers =====================
-unsigned int skyboxVAO = 0, skyboxVBO = 0;
-
-void renderSkyboxCube()
-{
-    if (skyboxVAO == 0)
-    {
-        float skyboxVertices[] = {
-            // 36 vertices for a cube
-            -1.0f,  1.0f, -1.0f,
-            -1.0f, -1.0f, -1.0f,
-             1.0f, -1.0f, -1.0f,
-             1.0f, -1.0f, -1.0f,
-             1.0f,  1.0f, -1.0f,
-            -1.0f,  1.0f, -1.0f,
-
-            -1.0f, -1.0f,  1.0f,
-            -1.0f, -1.0f, -1.0f,
-            -1.0f,  1.0f, -1.0f,
-            -1.0f,  1.0f, -1.0f,
-            -1.0f,  1.0f,  1.0f,
-            -1.0f, -1.0f,  1.0f,
-
-             1.0f, -1.0f, -1.0f,
-             1.0f, -1.0f,  1.0f,
-             1.0f,  1.0f,  1.0f,
-             1.0f,  1.0f,  1.0f,
-             1.0f,  1.0f, -1.0f,
-             1.0f, -1.0f, -1.0f,
-
-            -1.0f, -1.0f,  1.0f,
-            -1.0f,  1.0f,  1.0f,
-             1.0f,  1.0f,  1.0f,
-             1.0f,  1.0f,  1.0f,
-             1.0f, -1.0f,  1.0f,
-            -1.0f, -1.0f,  1.0f,
-
-            -1.0f,  1.0f, -1.0f,
-             1.0f,  1.0f, -1.0f,
-             1.0f,  1.0f,  1.0f,
-             1.0f,  1.0f,  1.0f,
-            -1.0f,  1.0f,  1.0f,
-            -1.0f,  1.0f, -1.0f,
-
-            -1.0f, -1.0f, -1.0f,
-            -1.0f, -1.0f,  1.0f,
-             1.0f, -1.0f, -1.0f,
-             1.0f, -1.0f, -1.0f,
-            -1.0f, -1.0f,  1.0f,
-             1.0f, -1.0f,  1.0f
-        };
-
-        glGenVertexArrays(1, &skyboxVAO);
-        glGenBuffers(1, &skyboxVBO);
-        glBindVertexArray(skyboxVAO);
-        glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    }
-
-    glBindVertexArray(skyboxVAO);
-    glDrawArrays(GL_TRIANGLES, 0, 36);
-    glBindVertexArray(0);
-}
-
-// ===================== Texture Loaders =====================
-unsigned int loadCubemap(std::vector<std::string> faces)
-{
-    unsigned int textureID;
-    glGenTextures(1, &textureID);
-    glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
-
-    int width, height, nrChannels;
-    for (unsigned int i = 0; i < faces.size(); i++)
-    {
-        unsigned char* data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
-        if (data)
-        {
-            GLenum format = (nrChannels == 3) ? GL_RGB : GL_RGBA;
-            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
-            stbi_image_free(data);
-        }
-        else
-        {
-            std::cerr << "Cubemap texture failed at: " << faces[i] << std::endl;
-            stbi_image_free(data);
-        }
-    }
-
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-
-    return textureID;
-}
-
+// ===================== Load Texture =====================
 unsigned int loadTexture(const char* path)
 {
     unsigned int textureID;
-    glGenTextures(1, &textureID);
+    glGenTextures(1,&textureID);
 
-    int width, height, nrChannels;
+    int width,height,nrChannels;
     stbi_set_flip_vertically_on_load(true);
-    unsigned char* data = stbi_load(path, &width, &height, &nrChannels, 0);
-
-    if (data)
+    unsigned char* data = stbi_load(path,&width,&height,&nrChannels,0);
+    if(data)
     {
         GLenum format = (nrChannels == 3) ? GL_RGB : GL_RGBA;
-        glBindTexture(GL_TEXTURE_2D, textureID);
+        glBindTexture(GL_TEXTURE_2D,textureID);
         glTexImage2D(GL_TEXTURE_2D,0,format,width,height,0,format,GL_UNSIGNED_BYTE,data);
         glGenerateMipmap(GL_TEXTURE_2D);
 
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
 
         stbi_image_free(data);
     }
@@ -200,58 +99,49 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
     glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "SolarSystem", NULL, NULL);
-    if(!window){ std::cout << "Failed to create window\n"; return -1; }
+
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH,SCR_HEIGHT,"SolarSystem",NULL,NULL);
+    if(!window){ std::cout<<"Failed to create window\n"; return -1; }
     glfwMakeContextCurrent(window);
-    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){ std::cout << "Failed to initialize GLAD\n"; return -1; }
 
-    glEnable(GL_DEPTH_TEST);
-
-    // Mouse and scroll callbacks
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     glfwSetCursorPosCallback(window, mouse_callback);
     glfwSetScrollCallback(window, scroll_callback);
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
-    // ===================== Shaders =====================
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){ std::cout<<"Failed to initialize GLAD\n"; return -1; }
+
+    glEnable(GL_DEPTH_TEST);
+
+    // --- Shaders ---
     Shader sunShader("shaders/emissive.vert","shaders/emissive.frag");
     Shader planetShader("shaders/lighting.vert","shaders/lighting.frag");
-    Shader skyboxShader("shaders/skybox.vert","shaders/skybox.frag");
 
-    // ===================== Textures =====================
+    // --- Textures ---
     unsigned int sunTex   = loadTexture("textures/sun.jpg");
     unsigned int earthTex = loadTexture("textures/earth.jpg");
     unsigned int moonTex  = loadTexture("textures/moon.jpg");
 
-    planetShader.use();
-    planetShader.setInt("texture1", 0); // Texture unit 0
-
-    std::vector<std::string> faces
-    {
-        "textures/skybox/right.jpg",
-        "textures/skybox/left.jpg",
-        "textures/skybox/top.jpg",
-        "textures/skybox/bottom.jpg",
-        "textures/skybox/front.jpg",
-        "textures/skybox/back.jpg"
-    };
-    unsigned int skyboxTex = loadCubemap(faces);
-
-    // ===================== Planets =====================
+    // --- Planets ---
     Planet sun(2.0f,64,64);
     Planet earth(0.5f,64,64);
     Planet moon(0.14f,32,32);
 
-    // ===================== Main Loop =====================
+    // --- Render Loop ---
     while(!glfwWindowShouldClose(window))
     {
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
         processInput(window);
 
-        float t = (float)glfwGetTime();
         glClearColor(0.01f,0.01f,0.01f,1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glm::mat4 view = camera.GetViewMatrix();
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom),(float)SCR_WIDTH/SCR_HEIGHT,0.1f,100.0f);
+        float t = (float)glfwGetTime();
 
         // --- Sun ---
         sunShader.use();
@@ -264,32 +154,23 @@ int main()
 
         // --- Earth ---
         planetShader.use();
+        planetShader.setInt("texture_diffuse1", 0);
         planetShader.setVec3("lightPos", glm::vec3(0,0,0));
         planetShader.setVec3("viewPos", camera.Position);
         planetShader.setMat4("view", view);
         planetShader.setMat4("projection", projection);
 
-        glm::vec3 earthPos = glm::vec3(10*cos(t),0,10*sin(t));
-        glm::mat4 earthModel = glm::translate(glm::mat4(1.0f), earthPos);
+        glm::mat4 earthModel = glm::translate(glm::mat4(1.0f), glm::vec3(10*cos(t),0,10*sin(t)));
         planetShader.setMat4("model", earthModel);
-        glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, earthTex);
         earth.draw();
 
         // --- Moon ---
+        glm::vec3 earthPos = glm::vec3(10*cos(t),0,10*sin(t));
         glm::mat4 moonModel = glm::translate(glm::mat4(1.0f), earthPos + glm::vec3(1*cos(t*2),0,1*sin(t*2)));
         planetShader.setMat4("model", moonModel);
         glBindTexture(GL_TEXTURE_2D, moonTex);
         moon.draw();
-
-        // --- Skybox ---
-        glDepthFunc(GL_LEQUAL);
-        skyboxShader.use();
-        skyboxShader.setMat4("view", glm::mat4(glm::mat3(view)));
-        skyboxShader.setMat4("projection", projection);
-        glBindTexture(GL_TEXTURE_CUBE_MAP, skyboxTex);
-        renderSkyboxCube();
-        glDepthFunc(GL_LESS);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
